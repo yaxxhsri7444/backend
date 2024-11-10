0b134b2de2166ee5635c418a60e20b631c3ba1a2
yash srivastava <yashlala716@gmail.com>
2024-11-10 23:04:44 +0530

    MangoDB connected

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// require('dotenv').config({path : './env'})
+
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+
+// First approach
+/*
+import express from "express";
+const app = express()(async () => {
+  try {
+    await mongoose.connect(`${process.env.MANGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.error("ERROR :", error);
+      throw err;
+    })
+
+    app.listen(process.env.PORT, () =>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+
+  } catch (error) {
+    console.error("ERROR :", error);
+    throw err;
+  }
+});
+*/
